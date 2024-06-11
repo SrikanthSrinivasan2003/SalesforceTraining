9daeca26edcafc7ac17d0048d5fe2b2d31b3fe96
Srikanth <154430800+SrikanthSrinivasan2003@users.noreply.github.com>
2024-06-11 20:43:26 +0530

    Create static methods.java

---
+++ TechnicalTraining/Day09/static methods.java
@@ -0,0 +1,34 @@
+public class MyClass {
+	private int Section;//non-static or instance variable
+	private static int srNo;//static 
+	
+	
+	//static block
+	static {
+		System.out.println("-----Within Static Block-----");
+		srNo++;
+	}
+	MyClass(){
+		System.out.println("----Within default constructor----");
+		srNo++;
+		Section++;
+	}
+	@Override
+	public String toString() {
+		return "employee [SerialNo=" + srNo + ", Section=" + Section +" ]";
+	}
+	static void display() {
+		//inside a static method you cannot access non static variable
+		//System.out.println("Section: "+Section);
+		System.out.println("SerialNo: "+srNo);
+	}
+}
+public class MyClassDemo {
+
+	public static void main(String[] args) {
+		MyClass obj1 = new MyClass();
+		System.out.println(obj1);
+		MyClass.display();
+	}
+
+}
